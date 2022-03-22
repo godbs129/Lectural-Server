58d482edc65b2d9862d19c058fab2e97212a885b
godbs129 <junhaeyoon@gmail.com>
2022-03-22 09:33:28 +0900

    Add: Response 추가

---
+++ src/lib/response/Response.ts
@@ -0,0 +1,13 @@
+export default class Response {
+  status: number;
+  message: string;
+
+  constructor(status: number, message: string) {
+    this.status = status;
+    this.message = message;
+  }
+
+  public static ok(message: string) {
+    return new Response(200, message);
+  }
+}
