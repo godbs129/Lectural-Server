bc116cfbb00027821d6791963e08dbdb86f83645
godbs129 <junhaeyoon@gmail.com>
2022-03-31 13:53:54 +0900

    Add: 위치 추가 api

--- src/api/place/place.controller.ts
+++ src/api/place/place.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { PlaceService } from './place.service';
 
 @Controller('place')
 export class PlaceController {
   constructor(private readonly placeService: PlaceService) {}
+
+  @Post('/')
+  public async getPlaceAndSave() {
+    this.placeService.getPlaceFromDodam();
+  }
 }
--- src/api/place/place.service.ts
+++ src/api/place/place.service.ts
@@ -1,10 +1,16 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import axios, { AxiosResponse } from 'axios';
+import { IPlace } from 'src/common/interfaces/IPlace';
 import { Place } from 'src/domain/entity/place.entity';
 import { PlaceRepository } from './repository/place.repository';
 
 @Injectable()
 export class PlaceService {
-  constructor(private readonly placeRepository: PlaceRepository) {}
+  constructor(
+    private readonly placeRepository: PlaceRepository,
+    private readonly configService: ConfigService,
+  ) {}
 
   public async getPlace(idx: number): Promise<Place> {
     const place: Place = await this.placeRepository.getPlace(idx);
@@ -14,5 +20,25 @@ export class PlaceService {
     }
 
     return place;
+  }
+
+  public async getPlaceFromDodam() {
+    const places: AxiosResponse = await axios.get(
+      `http://open.dodam.b1nd.com/api/place`,
+      {
+        headers: {
+          Authorization:
+            'Bearer ' + this.configService.get<string>('accessToken'),
+        },
+      },
+    );
+
+    places.data.data.map((place: IPlace) => {
+      this.savePlaces(place);
+    });
+  }
+
+  public savePlaces(place: IPlace) {
+    this.placeRepository.save(place);
   }
 }
