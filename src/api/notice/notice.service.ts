4ead0356da7a991b27858d894faac4099f563cb5
godbs129 <junhaeyoon@gmail.com>
2022-04-25 20:40:01 +0900

    ADD: 강의 삭제 공지 추가 기능

--- src/api/notice/notice.service.ts
+++ src/api/notice/notice.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { Lecture } from 'src/domain/entity/lecture.entity';
 import { Notice } from 'src/domain/entity/notice.entity';
 import { NoticeRepository } from './repository/notice.repository';
 
@@ -8,5 +9,12 @@ export class NoticeService {
 
   findByDate(): Promise<Notice[]> {
     return this.noticeRepository.findByDate(new Date());
+  }
+
+  createNotice(lecture: Lecture, expireDate: Date): void {
+    this.noticeRepository.save({
+      lecture: lecture,
+      expireDate: expireDate,
+    });
   }
 }
