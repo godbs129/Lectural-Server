117131c96c0d181a36f15638cd3d7da71c8811a1
godbs129 <junhaeyoon@gmail.com>
2022-04-27 13:10:45 +0900

    ADD: NoticeModule에 NoticeRepository import

--- src/api/notice/notice.module.ts
+++ src/api/notice/notice.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { NoticeService } from './notice.service';
 import { NoticeController } from './notice.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { NoticeRepository } from './repository/notice.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([NoticeRepository])],
   providers: [NoticeService],
-  controllers: [NoticeController]
+  controllers: [NoticeController],
 })
 export class NoticeModule {}
