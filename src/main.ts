665bb7c493d2fcf6eb6eefd281cce69aa69aad5e
godbs129 <junhaeyoon@gmail.com>
2022-05-02 11:31:11 +0900

    FIX: cors true

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 import { PORT } from './config/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(PORT, '0.0.0.0');
 }
